55275fe3271c905cdce234336c5e6ac5c2b3ee0d
eros <eros.ru@outlook.com>
2022-03-19 21:54:34 +0900

    fd

--- main.py
+++ main.py
@@ -176,6 +176,37 @@ if __name__ == '__main__':
                     # click to start download
                     btn_for_download.click()
 
+                    #time.sleep(1)
+
+                    # getting table data
+                    table_data = []
+                    table_element = driver.find_element(By.ID,'table_expedientes')
+                    rows = table_element.find_elements(By.TAG_NAME, "tr") # get all of the rows in the table
+                    for row in rows:
+                        # Get the columns (all the column 2)
+                        cols = row.find_elements(By.TAG_NAME, "td")
+
+                        if len(cols) != 2:
+                            continue
+
+                        expendidate = cols[0].text
+                        actor_demando = cols[1].text
+                        table_data.append([expendidate, actor_demando])
+                    try:
+                        table_element = driver.find_element(By.ID,'table_convocatoria')
+                        rows = table_element.find_elements(By.TAG_NAME, "tr") # get all of the rows in the table
+                        for row in rows:
+                            # Get the columns (all the column 2)
+                            cols = row.find_elements(By.TAG_NAME, "td")
+
+                            if len(cols) != 2:
+                                continue
+
+                            expendidate = cols[0].text
+                            actor_demando = cols[1].text
+                            table_data.append([expendidate, actor_demando])
+                    except:
+                        pass
                     # back to home page
                     driver.back()
 
@@ -185,9 +216,8 @@ if __name__ == '__main__':
                     for pdf_file in pdf_files:
                         print(pdf_file)
                         pdf_path = os.path.join(dir_path, 'download', pdf_file)
-                        if not read_save_pyminer(pdf_path, Laboral_Federal_Foraneos):
-                            if not read_save_fitz(pdf_path, Laboral_Federal_Foraneos):
-                                shutil.copyfile(pdf_path, os.path.join("hard", pdf_file))
+                        if not read_save_fitz_with_table_data(pdf_path, table_data,Laboral_Federal_Foraneos):
+                        	shutil.copyfile(pdf_path, os.path.join("hard", pdf_file))
                         #shutil.move(pdf_path, os.path.join("All", pdf_file))
                         os.remove(pdf_path)
 
